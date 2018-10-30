51f61bf73f15c863bc5a92a089c75455dba5f02f
D Hemken <dehemken@wisc.edu>
2018-10-30 15:20:52 -0500

    more data generating functions

--- gen_2x_f.r
+++ gen_2x_f.r
@@ -24,3 +24,25 @@ gen_2x_f <- function(nobs, means=c(0,0), sigma=0, nlevels=2L,
   
   return(data.frame(y, x1, x2, f))
 }
+
+
+gen_3x <- function(nobs=100L, means=c(0,0,0), sigma=diag(nrow=3,ncol=3), 
+                   coefs=c(rep(1L, times=8)),
+                   sd.resid=1) {
+  stopifnot(length(nobs)==1, is.integer(nobs))
+  stopifnot(length(means)==3, is.numeric(means))
+  stopifnot(is.matrix(sigma), is.numeric(sigma))
+  stopifnot(length(coefs)==8, is.numeric(coefs), !all(coefs==0))
+  stopifnot(length(sd.resid)==1, is.numeric(sd.resid), sd.resid>0)
+  
+  require(MASS)
+  
+  X <- as.data.frame(mvrnorm(nobs, means, Sigma=sigma))
+  x1 <- X[,1]; x2 <- X[,2]; x3 <- X[,3]
+  
+  mmat <- model.matrix(~x1*x2*x3)
+  
+  y <- mmat%*%(coefs) + rnorm(nobs, sd=sd.resid)
+  
+  return(data.frame(y, x1, x2, x3))
+}
--- stdParm factor vars.r
+++ stdParm factor vars.r
@@ -1,7 +1,7 @@
 source("stdParm functions.r")
 source("gen_2x_f.r")
 
-df <- gen_2x_f(100L)
+df <- gen_2x_f(100L, sigma=0.3)
 
 b <- coef(fit <- lm(y~ x1*x2*f, df))
 b.terms <- names(b)
