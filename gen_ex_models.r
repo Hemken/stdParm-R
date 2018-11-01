b9e92a9265a8bb39ef34e0f0fefa223e8ca843f5
Doug Hemken <dehemken@wisc.edu>
2018-11-01 08:21:28 -0500

    begin polynomials

--- gen_ex_models.r
+++ gen_ex_models.r
@@ -1,4 +1,6 @@
-# Two correlated x vars and a factor
+# gen_2x_f:    Two correlated x vars and a factor
+# gen_3x:      Three correlated x vars
+# gen_2x_poly: Two correlated x vars and x^2 terms
 
 gen_2x_f <- function(nobs, means=c(0,0), sigma=0, nlevels=2L,
                      coefs=c(rep(1L, times=4*nlevels)),
@@ -46,3 +48,24 @@ gen_3x <- function(nobs=100L, means=c(0,0,0), sigma=diag(nrow=3,ncol=3),
   
   return(data.frame(y, x1, x2, x3))
 }
+
+# gen_2x_poly <- function(nobs=100L, means=c(0,0), sigma=diag(nrow=2,ncol=2), 
+#                    coefs=c(rep(1L, times=8)),
+#                    sd.resid=1) {
+#   stopifnot(length(nobs)==1, is.integer(nobs))
+#   stopifnot(length(means)==3, is.numeric(means))
+#   stopifnot(is.matrix(sigma), is.numeric(sigma))
+#   stopifnot(length(coefs)==8, is.numeric(coefs), !all(coefs==0))
+#   stopifnot(length(sd.resid)==1, is.numeric(sd.resid), sd.resid>0)
+#   
+#   require(MASS)
+#   
+#   X <- as.data.frame(mvrnorm(nobs, means, Sigma=sigma))
+#   x1 <- X[,1]; x2 <- X[,2]; x3 <- X[,3]
+#   
+#   mmat <- model.matrix(~x1*x2*x3)
+#   
+#   y <- mmat%*%(coefs) + rnorm(nobs, sd=sd.resid)
+#   
+#   return(data.frame(y, x1, x2, x3))
+# }
