f20dc47cc8d70a55fd8f00a02305bc2988ceb771
Doug Hemken <dehemken@wisc.edu>
2018-11-14 08:05:45 -0600

    more than one factor variable

--- gen_ex_models.r
+++ gen_ex_models.r
@@ -1,4 +1,5 @@
 # gen_2x_f:    Two correlated x vars and a factor
+# gen_2x_2f:   Two correlated x vars and two factors
 # gen_3x:      Three correlated x vars
 # gen_2x_poly: Two correlated x vars and x^2 terms
 
@@ -25,6 +26,32 @@ gen_2x_f <- function(nobs, means=c(0,0), sigma=0, nlevels=2L,
   y <- mmat%*%(coefs) + rnorm(nobs, sd=sd.resid)
   
   return(data.frame(y, x1, x2, f))
+}
+
+gen_2x_2f <- function(nobs, means=c(0,0), sigma=0, nlevels=c(2L, 2L),
+                     coefs=c(rep(1L, times=4*prod(nlevels))),
+                     sd.resid=1) {
+  # factor has full interaction with the x variables
+  # coefs order=first order terms, second order terms, third order terms
+  stopifnot(length(nobs)==1, is.integer(nobs))
+  stopifnot(length(means)==2, is.numeric(means))
+  stopifnot(length(sigma)==1, is.numeric(sigma))
+  stopifnot(length(nlevels)==2, is.integer(nlevels), all(nlevels>=2))
+  stopifnot(length(coefs)==4*prod(nlevels), is.numeric(coefs), !all(coefs==0))
+  
+  require(MASS)
+  
+  Sigma <- matrix(c(1, sigma, sigma, 1), ncol=2)
+  X <- mvrnorm(nobs, means, Sigma=Sigma) # this is what MASS if for
+  x1 <- X[,1]; x2 <- X[,2]
+  
+  f1 <- as.factor(sample(letters[1:nlevels[1]], nobs, replace=TRUE))
+  f2 <- as.factor(sample(LETTERS[1:nlevels[2]], nobs, replace=TRUE))
+  mmat <- model.matrix(~x1*x2*f1*f2)
+  
+  y <- mmat%*%(coefs) + rnorm(nobs, sd=sd.resid)
+  
+  return(data.frame(y, x1, x2, f1, f2))
 }
 
 
--- stdParm factor vars.r
+++ stdParm factor vars.r
@@ -2,31 +2,45 @@ source("stdParm functions.r")
 source("gen_ex_models.r")
 
 df <- gen_2x_f(100L, sigma=0.3)
+# two x variables, x1 and x2, and one factor
+#   variable, f, with levels a and b
 
-b <- coef(fit <- lm(y~ x1*x2+f*x1, df))
+# Original model, to be centered and scaled
+summary(fit <- lm(y~ x1*x2+f*x1, df))
+
+# extract coefficients, term names
+b <- coef(fit)
 b.terms <- names(b)
 b
 
-summary(fit)
+# means and sds of continuous variables
+x.means <- colMeans(df[,c("x1","x2")])
+x.sds <- colSds(df[,c("x1","x2")])
 
+# recentering and rescaling matrices
+C <- matrix.build.clean(x.means, b.terms)
+S <- matrix.build.clean(x.sds, b.terms, type="scale")
+
+# combine to standardize
+Z <- S %*% C # the order is crucial
+Z
+
+###################################
+# Now combine with factor variables
+###################################
+
+# number of levels, term names
 fln <- nlevels(df$f)
 fnames    <- paste0("f", levels(df$f))
 fnames[1] <- "(Intercept)"
 
-x.means <- colMeans(df[,c("x1","x2")])
-x.sds <- colSds(df[,c("x1","x2")])
-
-C <- matrix.build.clean(x.means, b.terms)
-S <- matrix.build.clean(x.sds, b.terms, type="scale")
-
-Z <- S %*% C # the order is crucial
-Z
- 
+# given the factor term names, build Z
 Z.plus <- factor.direct.sum(Z,fnames)
 found <- matching.terms(colnames(Z.plus), b.terms)
 Z <- Z.plus[found,found]
 Z
 
+# Now use the result, and check it
 # x-standardized
 b.x <- Z%*%b
 b.x
