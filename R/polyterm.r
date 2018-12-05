fd743290a288d7c36e3acb7d393d014ebd7dc3b3
D Hemken <dehemken@wisc.edu>
2018-12-05 15:06:27 -0600

    rescaling polynomials

--- R/polyterm.r
+++ R/polyterm.r
@@ -1,4 +1,4 @@
-polyterm <- function(mu, b.terms){
+polyterm <- function(mu, b.terms, type="center"){
   stopifnot(is.numeric(mu), length(mu)==1, length(names(mu))==1,
             is.character(b.terms))
   
@@ -6,7 +6,11 @@ polyterm <- function(mu, b.terms){
 #  print(term.vars)
   polydegree <- max(term.vars[,names(mu)])
   
-  A <- C <- mean.to.matrix(mu) # recentering x - 3
+  if (type=="center") {
+    A <- C <- mean.to.matrix(mu) # recentering x - 3
+  } else if (type=="scale") {
+    A <- C <- sd.to.matrix(mu)
+  }
   if (polydegree > 1){
     for (i in 2:polydegree){
       C <- kron(A,C)
@@ -18,17 +22,22 @@ polyterm <- function(mu, b.terms){
   cnames <- colnames(C)
   keepcols <- match(b.terms, cnames)
   C <- C[, keepcols]
-  colnames(C) <- cnames[keepcols]
+#  colnames(C) <- cnames[keepcols] # redundant?
 #  print(C)
   
-  # collect repeated row terms
-  cnames <- colnames(C) # reduced columns
-  rnames <- rownames(C)
-  rowcombine <- matrix(NA, nrow=length(cnames), ncol=length(rnames))
-  for (i in 1:length(cnames)) {
-    rowcombine[i,] <- rnames %in% cnames[i]
+  if (type=="center") {
+    # collect repeated row terms
+    cnames <- colnames(C) # reduced columns
+    rnames <- rownames(C)
+    rowcombine <- matrix(NA, nrow=length(cnames), ncol=length(rnames))
+    for (i in 1:length(cnames)) {
+      rowcombine[i,] <- rnames %in% cnames[i]
+    }
+    C <- rowcombine %*% C
+    rownames(C) <- cnames
+  } else if (type=="scale") {
+    C <- C[keepcols, ] # keep the same rows as columns
   }
-  C <- rowcombine %*% C
-  rownames(C) <- cnames
+  
   return(C)
 }
